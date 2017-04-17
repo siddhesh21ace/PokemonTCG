d2a11ca4eac9598acffc9a5077e1bac85fa5e06d
tintin1343 <nitin1343@gmail.com>
2017-04-16 22:27:12 -0400

    Add leaderboard

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 
-function main() {
+// function main() {
 
 (function () {
    'use strict';
@@ -104,9 +104,11 @@ function main() {
   // jQuery Parallax
   function initParallax() {
     $('#intro').parallax("100%", 0.3);
-    $('#services').parallax("100%", 0.3);
-    $('#aboutimg').parallax("100%", 0.3);	
-    $('#testimonials').parallax("100%", 0.1);
+      $('#about').parallax("100%", 0.3);
+    $('#pokemon').parallax("100%", 0.3);
+    $('#pokedex').parallax("100%", 0.3);
+    $('#gym').parallax("100%", 0.1);
+      $('#leaderboard').parallax("100%", 0.1);
 
   }
   initParallax();
@@ -114,10 +116,42 @@ function main() {
   	// Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false
-	});	
+	});
+
+    var theater = theaterJS();
+    theater
+        .on('type:start, erase:start', function () {
+            // add a class to actor's dom element when he starts typing/erasing
+            var actor = theater.getCurrentActor()
+            actor.$element.classList.add('is-typing')
+        })
+        .on('type:end, erase:end', function () {
+            // and then remove it when he's done
+            var actor = theater.getCurrentActor()
+            actor.$element.classList.remove('is-typing')
+        })
+
+
+    theater
+        .addActor('vader', 0.8)
+
+    theater
+        .addScene('vader:Pikachu...', 400)
+        .addScene('vader:Bulbasaur...', 400)
+        .addScene('vader:Venusaur...', 400)
+        .addScene('vader:Ivysaur...', 400)
+        .addScene('vader:Charmeleon...', 400)
+        .addScene('vader:Blastoise...', 400)
+        .addScene('vader:Kakuna...', 400)
+        .addScene('vader:caterpie...', 400)
+        .addScene('vader:Pikachu...', 400)
+        .addScene('vader:wartortle...', 400)
+        .addScene('vader:beedrill...', 400)
+        .addScene('vader:rattata...', 400)
+        .addScene(theater.replay);
 
 }());
 
 
-}
-main();
+// }
+// main();
